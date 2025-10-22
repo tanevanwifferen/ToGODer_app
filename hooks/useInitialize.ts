eecf1e412c863960a7732213f7309b792d4e2b47
Tane van Wifferen <tanevanwifferen@gmail.com>
2025-10-22 15:29:19 +0200

    try this

--- hooks/useInitialize.ts
+++ hooks/useInitialize.ts
@@ -11,6 +11,9 @@ import { InitializationService } from "../services/InitializationService";
 import { useSelector } from "react-redux";
 
 export function useInitialize() {
+  const oldGlobalConfig = useSelector(selectGlobalConfig);
+  const defaultModel = useSelector(selectDefaultModel);
+
   const initializeApp = useCallback(async () => {
     try {
       // Initialize auth and API services
@@ -19,8 +22,6 @@ export function useInitialize() {
       // Fetch global config
       const globalConfig = await GlobalApiClient.getGlobalConfig();
       store.dispatch(setGlobalConfig(globalConfig));
-      const oldGlobalConfig = useSelector(selectGlobalConfig);
-      const defaultModel = useSelector(selectDefaultModel);
       if (
         defaultModel != oldGlobalConfig.previousDefaultModel &&
         defaultModel != ""
